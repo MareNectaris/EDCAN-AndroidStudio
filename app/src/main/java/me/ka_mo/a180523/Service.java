a6768e5f2ff65f5599ce15615a8baee6ff7cfadb
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2018-05-23 16:30:23 +0900

    minor bugfix

--- app/src/main/java/me/ka_mo/a180523/Service.java
+++ app/src/main/java/me/ka_mo/a180523/Service.java
@@ -10,13 +10,13 @@ public class Service extends android.app.Service {
     BroadcastReceiver broadcastReceiver;
     @Override
     public void onDestroy(){
-        unregisterReceiver(broadcastReceiver);
         super.onDestroy();
+        unregisterReceiver(broadcastReceiver);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        BroadcastReceiver broadcastReceiver = new Broadcast();
+        broadcastReceiver = new Broadcast();
         IntentFilter intentFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
         registerReceiver(broadcastReceiver,intentFilter);
         return super.onStartCommand(intent, flags, startId);
