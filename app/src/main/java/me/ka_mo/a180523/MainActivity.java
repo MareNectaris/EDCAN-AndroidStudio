392d5a1d878e12503dba39eb9f9eaaed8756ed94
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2018-05-23 16:27:49 +0900

    180523

--- app/src/main/java/me/ka_mo/a180523/MainActivity.java
+++ app/src/main/java/me/ka_mo/a180523/MainActivity.java
@@ -5,21 +5,32 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this,Service.class);
+        intent = new Intent(this,Service.class);
         startService(intent);
     }
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
+    }
+
+    public void on(View v){
+        startService(intent);
+        Toast.makeText(getApplicationContext(),"켜짐",Toast.LENGTH_SHORT).show();
+    }
+    public void off(View v){
+        stopService(intent);
+        Toast.makeText(getApplicationContext(),"꺼짐",Toast.LENGTH_SHORT).show();
     }
 }
